5e53f4e95a0a4f915eddf98b93d708760cc69e97
KRyderTR <charitrom@gmail.com>
2025-01-27 00:39:46 +0200

    infoPage-update

--- src/data/objects.js
+++ src/data/objects.js
@@ -4,13 +4,13 @@ import htpImage from "../assets/images/htp-page-image.png";
 const data = {
     howToPlay: {
       title: "How to Play",
-      text: "This is a guide on how to play the animal game. Follow these steps to succeed!",
-      image: htpImage, // Replace with an actual path
+      text: "Welcome to the Animal English Game! Click the Start Game button to begin. You’ll see a picture of an animal on the screen. For each animal, you’ll have 4 answer options displayed below the image. Each option has a sound button that spells out the animal’s name in English. Tip: Tap the sound button to hear the pronunciation before choosing your answer!. For every correct answer, you’ll earn a point! Can you guess all the animals correctly🐾✨?",
+      image: htpImage, 
     },
     about: {
       title: "About",
       text: "The Animal Game is a delightful and educational adventure created to inspire young learners, especially my 4-year-old niece, who loves animals and is eager to learn English. This game helps kids explore the animal kingdom while improving their language skills in a fun and interactive way. It's designed with care to make learning an exciting experience for children of all ages.",
-      image: aboutImage, // Replace with an actual path
+      image: aboutImage, 
     },
     
   };
